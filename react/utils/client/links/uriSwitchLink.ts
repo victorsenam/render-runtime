0c5e0f2833b4a0047db23f0e356daba8d9b947ed
Tiago Gimenes <tlgimenes@gmail.com>
2018-03-20 01:19:59 -0300

    last work before change

--- react/utils/client/links/uriSwitchLink.ts
+++ react/utils/client/links/uriSwitchLink.ts
@@ -1,68 +1,34 @@
 import {ApolloLink, NextLink, Observable, Operation, RequestHandler} from 'apollo-link'
 import {visit} from 'graphql'
 
-const versionVisitorExtractor = (versionHolder: any) => {
-  return {
-    Argument (node: any) {
-      if (node.name.value === 'version') {
-        versionHolder.version = node.value.value
-      }
+const versionExtractorVisitor = (assets: any) => ({
+  Argument (node: any) {
+    if (node.name.value === 'version') {
+      assets.version = node.value.value
     }
   }
-}
-
-const versionFromQuery = (query: any) => {
-  const versionHolder = {version: '0'}
-  const versionExtractor = versionVisitorExtractor(versionHolder)
-  visit(query, versionExtractor)
-  return versionHolder.version
-}
-
-const uriFromQuery = (query: any) => `/_v/v${versionFromQuery(query)}/graphql`
-
-export const uriSwitchLink = new ApolloLink((operation: Operation, forward?: NextLink) => {
-  const context = operation.getContext()
-  const {query} = operation
-
-  operation.setContext({
-    ...context,
-    uri: uriFromQuery(query)
-  })
-
-  return forward ? forward(operation) : null
 })
 
-/*
- * This linker middleware is used for changing http verbs
- *
- * If the query type is Query, the request will be made using an HTTP GET
- * If the query type is Mutation, the request will be made using HTTP POST
- */
-// export const createHttpSwitchLink = (inputURI) => {
-//   return new ApolloLink((operation: Operation, forward: NextLink) => {
-//     const {query, variables, operationName} = operation
-//     const context = operation.getContext()
-//     const {fetchOptions = {}, uri: contextURI} = context
-//     const targetUri: any = parse(contextURI || inputURI, true)
-//     delete targetUri.search
+const scopeExtractorVisitor = (assets: any) => ({
+  Argument (node: any) {
+    if (node.name.value === 'scope') {
+      assets.scope = node.value.value
+    }
+  }
+})
 
-//     fetchOptions.method = hasMutationField(query) ? 'POST' : 'GET'
+const uriFromQuery = (query: any) => {
+  const assets = {version: '1', scope: 'public'}
+  visit(query, versionExtractorVisitor(assets))
+  visit(query, scopeExtractorVisitor(assets))
 
-//     if(fetchOptions.method === 'GET') {
-//       targetUri.query = {
-//         ...targetUri.query,
-//         operationName,
-//         'query': print(query).replace(/\s\s+/g, ' '),
-//         variables,
-//       }
-//     }
+  return `/_v/v${assets.version}/${assets.scope}/graphql`
+}
 
-//     operation.setContext({
-//       ...context,
-//       fetchOptions,
-//       uri: format(targetUri),
-//     })
-
-//     return forward(operation)
-//   })
-// }
+export const uriSwitchLink = new ApolloLink((operation: Operation, forward?: NextLink) => {
+  operation.setContext({
+    ...operation.getContext(),
+    uri: uriFromQuery(operation.query)
+  })
+  return forward ? forward(operation) : null
+})
